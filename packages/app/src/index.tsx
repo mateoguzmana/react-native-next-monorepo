cbc474a06af73ac0ecbfa1a0202784f744dd08d2
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-01 22:40:42 +0200

    Pairing up linters and tsconfig from base files.

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export function App() {
   return (
@@ -17,37 +17,37 @@ export function App() {
 
 const styles = StyleSheet.create({
   root: {
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
   logo: {
     width: 120,
     height: 120,
-    marginBottom: 20,
+    marginBottom: 20
   },
   text: {
     fontSize: 28,
-    fontWeight: "600",
+    fontWeight: '600'
   },
   platformRow: {
     marginTop: 12,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   platformValue: {
     fontSize: 28,
-    fontWeight: "500",
+    fontWeight: '500'
   },
   platformBackground: {
-    backgroundColor: "#ececec",
+    backgroundColor: '#ececec',
     borderWidth: StyleSheet.hairlineWidth,
-    borderColor: "#d4d4d4",
+    borderColor: '#d4d4d4',
     paddingHorizontal: 6,
     borderRadius: 6,
-    alignItems: "center",
-  },
+    alignItems: 'center'
+  }
 });
 
 export default App;

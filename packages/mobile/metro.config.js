3d02a3b3bc0d1550c339abf8a36bf570127f296e
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-01 16:19:18 +0200

    Setting up monorepo step one: mobile and app

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,23 @@
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {getMetroTools} = require('react-native-monorepo-tools');
+
+const monorepoMetroTools = getMetroTools();
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  // Add additional Yarn workspace package roots to the module map.
+  // This allows importing importing from all the project's packages.
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    // Ensure we resolve nohoist libraries from this directory.
+    blockList: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
+  },
+};
